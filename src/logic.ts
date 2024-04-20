tojatomasz/name-that-tune

commit 892d3f1e2c1f4bd5690a20298b12fd3fafdebdb8

Fixes for ESLint guidelines

src/logic.ts
@@ -9,7 +9,7 @@ export const getSettings = (): appSettings => {
   console.log('Getting settings at getSettings()');
   const settings = getLocalStorageDataFromKey(SETTINGS_KEY, {}) as appSettings;
   //if settings are not set, set them to default
-  if(!settings.similarityRequirement || !settings.hintSetting || !settings.inputMethod) {
+  if (!settings.similarityRequirement || !settings.hintSetting || !settings.inputMethod) {
     console.log('Settings not set, setting to default at getSettings()');
     return setSettingsToDefault();
   }
@@ -32,12 +32,12 @@ export const setSettingsToDefault = () => {
 
 export const goBackToGame = () => {
   Spicetify.Platform.History.push({
-      pathname: '/name-that-tune/game',
-      state: {
-        data: {
-        },
+    pathname: '/name-that-tune/game',
+    state: {
+      data: {
       },
-    });
+    },
+  });
 };
 
 export const resetStats = () => {
@@ -61,21 +61,21 @@ export const getRandomTrackTitles = (next: boolean): string[] => {
     const title = tracks[index].contextTrack.metadata.title;
     return normalize(title, true);
   });
-  
+
   const currentTrackTitle = Spicetify.Player.data.item?.metadata?.title;
-  if(!Spicetify.Player.data.item) {return []}
+  if (!Spicetify.Player.data.item) {return [];}
   const nextTrackTitle = Spicetify.Queue.nextTracks[0].contextTrack.metadata.title;
-  if(!Spicetify.Queue.nextTracks[0].contextTrack) {return []}
+  if (!Spicetify.Queue.nextTracks[0].contextTrack) {return [];}
 
   console.log('Current title: '+currentTrackTitle);
-  if(Spicetify.Queue.prevTracks[0]) {
+  if (Spicetify.Queue.prevTracks[0]) {
     console.log('Previous title: '+Spicetify.Queue.prevTracks[0].contextTrack.metadata.title);
   }
 
   //when we open the game, the currentTrackTitle is current track, but when we click next, the currentTrackTitle is the previous track
-  if(next){
+  if (next) {
     titles.push(normalize(nextTrackTitle, true));
-  }else{
+  } else {
     titles.push(normalize(currentTrackTitle, true));
   }
   console.log('Tracks: '+titles);
@@ -108,9 +108,9 @@ const normalize = (str: string | undefined, keepSpaces: boolean = false) => {
 
   // Remove everything that is not a number, letter, Cyrylic alphabet, Polish alphabet or space
   cleaned = cleaned.replace(/[^\wа-яА-ЯіїІЇ\dąćęłńóśźż\s]/g, '');
-  
+
   // Remove spaces
-  if(!keepSpaces) cleaned = cleaned.replace(/\s/g, '');
+  if (!keepSpaces) cleaned = cleaned.replace(/\s/g, '');
 
   // TODO: add any other logic?
 
@@ -127,39 +127,39 @@ export const showHint = (hint: number) => {
   const hintSetting = getSettings().hintSetting;
   console.log('Hint setting: '+{ hintSetting });
   let updatedHint = '';
+  let nonSpaceChars = 0;
+  const words = currentHint.split(' ');
 
   switch (hintSetting) {
-    case 'oneLetter':
-      let nonSpaceChars = 0;
-      updatedHint = currentHint
-        .split('')
-        .map((char) => {
-          if (char !== ' ') {
-            return nonSpaceChars++ < hint ? char : '*';
-          }
-          return ' ';
-        })
-        .join('');
-      break;
-    case 'oneWord':
-      const words = currentHint.split(' ');
-      updatedHint = words
-        .map((word, index) => (index < hint ? word : '*'.repeat(word.length)))
-        .join(' ');
-      break;
-    case 'oneLetterOnEachWord':
-      updatedHint = currentHint
-        .split(' ')
-        .map((word) =>
-          word
-            .split('')
-            .map((char, index) => (index < hint ? char : '*'))
-            .join('')
-        )
-        .join(' ');
-      break;
-    default:
-      break;
+  case 'oneLetter':
+    updatedHint = currentHint
+      .split('')
+      .map((char) => {
+        if (char !== ' ') {
+          return nonSpaceChars++ < hint ? char : '*';
+        }
+        return ' ';
+      })
+      .join('');
+    break;
+  case 'oneWord':
+    updatedHint = words
+      .map((word, index) => (index < hint ? word : '*'.repeat(word.length)))
+      .join(' ');
+    break;
+  case 'oneLetterOnEachWord':
+    updatedHint = currentHint
+      .split(' ')
+      .map((word) =>
+        word
+          .split('')
+          .map((char, index) => (index < hint ? char : '*'))
+          .join(''),
+      )
+      .join(' ');
+    break;
+  default:
+    break;
   }
 
   return updatedHint;
@@ -175,7 +175,7 @@ export const checkSimilarity = (guess: string) => {
   const similarity = diceCoefficient(normalizedGuess, normalizedTitle);
   console.log({ similarity });
   return similarity;
-}
+};
 
 export const checkGuess = (guess: string) => {
   console.log({

src/pages/Game.tsx
@@ -74,11 +74,11 @@ class Game extends React.Component<
 
   guessChange = (e: React.ChangeEvent<HTMLInputElement>) =>
     this.setState({ guess: e.target.value });
-  
+
   updateSimilarity = () => {
-      const similarityScore = (checkSimilarity(this.state.guess) * 100).toFixed(0);
-      this.setState({ similarity: similarityScore.toString() + '%' });
-    };
+    const similarityScore = (checkSimilarity(this.state.guess) * 100).toFixed(0);
+    this.setState({ similarity: similarityScore.toString() + '%' });
+  };
 
   skipGuess = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
@@ -122,11 +122,11 @@ class Game extends React.Component<
         this.audioManager.setEnd(stageToTime(this.state.stage));
       }
     });
-    };
+  };
 
   giveHint = (e: React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
-      this.setState({hint: showHint(this.state.hintCount++) || ''})
+    e.preventDefault();
+    this.setState((prevState) => ({ hint: showHint(prevState.hintCount + 1) || '' }));
   };
 
   giveUp = () => {
@@ -147,7 +147,7 @@ class Game extends React.Component<
     Spicetify.Player.seek(0);
     Spicetify.Player.pause();
     this.audioManager.setEnd(1);
-    
+
     this.setState({
       guesses: [],
       // Reset the guess
@@ -202,7 +202,6 @@ class Game extends React.Component<
     const keyboardInput = this.state.settings.inputMethod === 'keyboard';
     const isPlaying = this.state.gameState === GameState.Playing;
     const { t } = this.props;
-    
 
     return (
       <>
@@ -212,41 +211,41 @@ class Game extends React.Component<
           <h2 className={styles.hint}>{(this.state.hint)}</h2>
           <h2 className={styles.similarity}>{(this.state.similarity)}</h2>
           {keyboardInput &&(
-          <form onSubmit={this.submitGuess}>
-            <input
-              type={'text'}
-              className={styles.input}
-              placeholder={t('guessPlaceholder') as string}
-              value={this.state.guess}
-              disabled={!isPlaying}
-              onChange={this.guessChange}
-            />
-            <div className={styles.formButtonContainer}>
-              <Button onClick={this.submitGuess} disabled={!isPlaying}>
-                {t('guessBtn')}
-              </Button>
-              <Button onClick={this.giveHint} disabled={!isPlaying}>
-                {t('hintBtn')}
-              </Button>
-              <Button onClick={this.skipGuess} disabled={!isPlaying}>
-                {t('skipBtn')}
-              </Button>
-            </div>
-          </form>
+            <form onSubmit={this.submitGuess}>
+              <input
+                type={'text'}
+                className={styles.input}
+                placeholder={t('guessPlaceholder') as string}
+                value={this.state.guess}
+                disabled={!isPlaying}
+                onChange={this.guessChange}
+              />
+              <div className={styles.formButtonContainer}>
+                <Button onClick={this.submitGuess} disabled={!isPlaying}>
+                  {t('guessBtn')}
+                </Button>
+                <Button onClick={this.giveHint} disabled={!isPlaying}>
+                  {t('hintBtn')}
+                </Button>
+                <Button onClick={this.skipGuess} disabled={!isPlaying}>
+                  {t('skipBtn')}
+                </Button>
+              </div>
+            </form>
           )}
           {!keyboardInput && (
-          <div className={styles.formButtonContainer}>
-          {this.state.randomTitles.map((title, index) => (
-              <Button
-                key={index}
-                classes={[styles.titleButton]}
-                onClick={this.handleRandomTitleButtonClick}
-                disabled={!isPlaying}
-              >
-                {title}
-              </Button>
-            ))}
-          </div>)}
+            <div className={styles.formButtonContainer}>
+              {this.state.randomTitles.map((title, index) => (
+                <Button
+                  key={index}
+                  classes={[styles.titleButton]}
+                  onClick={this.handleRandomTitleButtonClick}
+                  disabled={!isPlaying}
+                >
+                  {title}
+                </Button>
+              ))}
+            </div>)}
 
           {isPlaying ? (
             <Button

src/app.tsx
@@ -12,7 +12,7 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 
 import Game from './pages/Game';
 import Stats from './pages/Stats';
-import Settings from './pages/Settings'; 
+import Settings from './pages/Settings';
 
 import './css/app.global.scss';
 
@@ -53,7 +53,7 @@ class App extends React.Component {
     }
     // Otherwise, render the main Game
     else {
-    return <Game t={t} />;
+      return <Game t={t} />;
     }
   }
 }

src/pages/Settings.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { TFunction } from 'i18next';
-import { getLocalStorageDataFromKey} from '../Utils';
+import { getLocalStorageDataFromKey } from '../Utils';
 import { goBackToGame, setSettingsToDefault } from '../logic';
 import { SETTINGS_KEY } from '../constants';
 import { appSettings } from '../types/settings';
@@ -8,109 +8,106 @@ import Button from '../components/Button';
 import styles from '../css/app.module.scss';
 
 class Settings extends React.Component<{ t: TFunction }> {
-    state = {
-        inputMethod: '',
-        hintSetting: '',
-        similarityRequirement: 0,
+  state = {
+    inputMethod: '',
+    hintSetting: '',
+    similarityRequirement: 0,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputMethod: '',
+      hintSetting: '',
+      similarityRequirement: 0,
     };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            inputMethod: '',
-            hintSetting: '',
-            similarityRequirement: 0,
-        };
-    }
+  componentDidMount() {
+    const savedSettings = getLocalStorageDataFromKey(SETTINGS_KEY, {}) as appSettings;
+    this.setState({
+      inputMethod: savedSettings.inputMethod,
+      hintSetting: savedSettings.hintSetting,
+      similarityRequirement: savedSettings.similarityRequirement,
+    });
+  }
 
-    componentDidMount() {
-        const savedSettings = getLocalStorageDataFromKey(SETTINGS_KEY, {}) as appSettings;
-        this.setState({
-            inputMethod: savedSettings.inputMethod,
-            hintSetting: savedSettings.hintSetting,
-            similarityRequirement: savedSettings.similarityRequirement,
-        });
-    }
+  handleinputMethodChange = (event) => {
+    this.setState({ inputMethod: event.target.value }, () => {
+      this.saveSettingsToLocalStorage();
+    });
+  };
 
-    handleinputMethodChange = (event) => {
-        this.setState({ inputMethod: event.target.value }, () => {
-            this.saveSettingsToLocalStorage();
-        });
-    }
+  handleHintSettingChange = (event) => {
+    this.setState({ hintSetting: event.target.value }, () => {
+      this.saveSettingsToLocalStorage();
+    });
+  };
 
-    handleHintSettingChange = (event) => {
-        this.setState({ hintSetting: event.target.value }, () => {
-            this.saveSettingsToLocalStorage();
-        });
+  handlesimilarityRequirementChange = (event) => {
+    this.setState({ similarityRequirement: event.target.value }, () => {
+      this.saveSettingsToLocalStorage();
+    });
+  };
+
+  saveSettingsToLocalStorage = () => {
+    const { inputMethod, hintSetting, similarityRequirement } = this.state;
+    const updatedSettings = {
+      inputMethod,
+      hintSetting,
+      similarityRequirement,
     };
+    localStorage.setItem(SETTINGS_KEY, JSON.stringify(updatedSettings));
+  };
 
-    handlesimilarityRequirementChange = (event) => {
-        this.setState({ similarityRequirement: event.target.value }, () => {
-            this.saveSettingsToLocalStorage();
-        });
-    };
-
-    saveSettingsToLocalStorage = () => {
-        const { inputMethod, hintSetting, similarityRequirement } = this.state;
-        const savedSettings = getLocalStorageDataFromKey(SETTINGS_KEY, {}) as appSettings;
-        const updatedSettings = {
-            inputMethod,
-            hintSetting,
-            similarityRequirement,
-        };
-        localStorage.setItem(SETTINGS_KEY, JSON.stringify(updatedSettings));
-    };
-
-
-
-    render() {
-        const { t } = this.props;
-        return (
-            <>
-                <div className={styles.container}>
-                    <h1 className={styles.title}>{t('title')}</h1>
-                    <h2>{t('settings.title')}</h2>
-                    <Button onClick={goBackToGame} classes={[styles.goBackToGameButton]}>
-                        {t('gobacktogamebutton')}
-                    </Button>
-                    <div className={styles.settingsContainer}>
-                        <div className={styles.settingsRow}>
-                            <label className={styles.settingsLabel}>{t('settings.inputMethod')}</label>
-                            <select className={styles.settingsSelect} value={this.state.inputMethod} onChange={this.handleinputMethodChange}>
-                                <option className={styles.settingsOption} value="keyboard">{t('settings.inputKeyboard')}</option>
-                                <option className={styles.settingsOption} value="buttons">{t('settings.inputButtons')}</option>
-                            </select>
-                        </div>
-                        <div className={styles.settingsRow}>
-                            <label className={styles.settingsLabel}>{t('settings.hintSetting')}</label>
-                            <select className={styles.settingsSelect} value={this.state.hintSetting} onChange={this.handleHintSettingChange}>
-                                <option className={styles.settingsOption} value="oneLetter">{t('settings.hintOneLetter')}</option>
-                                <option className={styles.settingsOption} value="oneWord">{t('settings.hintOneWord')}</option>
-                                <option className={styles.settingsOption} value="oneLetterOnEachWord">{t('settings.hintOneLetterOnEachWord')}</option>
-                            </select>
-                        </div>
-                        <div className={styles.settingsRow}>
-                            <label className={styles.settingsLabel}>{t('settings.similarityRequirement')}</label>
-                            <input
-                                type="range"
-                                id="similarityRequirement"
-                                min={0}
-                                max={1.0}
-                                step={0.01}
-                                value={this.state.similarityRequirement}
-                                onChange={this.handlesimilarityRequirementChange}
-                                className={styles.settingsInput}
-                            />
-                            <span className={styles.similarityDisplay}>{t('settings.similarityPercentage', { percentage: (this.state.similarityRequirement * 100).toFixed(0)})}</span>
-                        </div>
-                    </div>
-                    <Button onClick={setSettingsToDefault} classes={[styles.resetButton]}>
-                        {t('settings.resetSettingsButton')}
-                    </Button>
-                </div>
-            </>
-        );
-    }
+  render() {
+    const { t } = this.props;
+    return (
+      <>
+        <div className={styles.container}>
+          <h1 className={styles.title}>{t('title')}</h1>
+          <h2>{t('settings.title')}</h2>
+          <Button onClick={goBackToGame} classes={[styles.goBackToGameButton]}>
+            {t('gobacktogamebutton')}
+          </Button>
+          <div className={styles.settingsContainer}>
+            <div className={styles.settingsRow}>
+              <label className={styles.settingsLabel}>{t('settings.inputMethod')}</label>
+              <select className={styles.settingsSelect} value={this.state.inputMethod} onChange={this.handleinputMethodChange}>
+                <option className={styles.settingsOption} value="keyboard">{t('settings.inputKeyboard')}</option>
+                <option className={styles.settingsOption} value="buttons">{t('settings.inputButtons')}</option>
+              </select>
+            </div>
+            <div className={styles.settingsRow}>
+              <label className={styles.settingsLabel}>{t('settings.hintSetting')}</label>
+              <select className={styles.settingsSelect} value={this.state.hintSetting} onChange={this.handleHintSettingChange}>
+                <option className={styles.settingsOption} value="oneLetter">{t('settings.hintOneLetter')}</option>
+                <option className={styles.settingsOption} value="oneWord">{t('settings.hintOneWord')}</option>
+                <option className={styles.settingsOption} value="oneLetterOnEachWord">{t('settings.hintOneLetterOnEachWord')}</option>
+              </select>
+            </div>
+            <div className={styles.settingsRow}>
+              <label className={styles.settingsLabel}>{t('settings.similarityRequirement')}</label>
+              <input
+                type="range"
+                id="similarityRequirement"
+                min={0}
+                max={1.0}
+                step={0.01}
+                value={this.state.similarityRequirement}
+                onChange={this.handlesimilarityRequirementChange}
+                className={styles.settingsInput}
+              />
+              <span className={styles.similarityDisplay}>{t('settings.similarityPercentage', { percentage: (this.state.similarityRequirement * 100).toFixed(0) })}</span>
+            </div>
+          </div>
+          <Button onClick={setSettingsToDefault} classes={[styles.resetButton]}>
+            {t('settings.resetSettingsButton')}
+          </Button>
+        </div>
+      </>
+    );
+  }
 }
 
 export default Settings;

src/pages/Stats.tsx
@@ -128,7 +128,7 @@ class Stats extends React.Component<{ t: TFunction }> {
           <h1 className={styles.title}>{t('title')}</h1>
           <h2>{t('stats.title')}</h2>
           <Button onClick={goBackToGame} classes={[styles.goBackToGameButton]}>
-                        {t('gobacktogamebutton')}
+            {t('gobacktogamebutton')}
           </Button>
           <p>{t('stats.winPercentage', { percentage: (winPercentage * 100).toFixed(2) })}</p>
           <table>
@@ -152,7 +152,7 @@ class Stats extends React.Component<{ t: TFunction }> {
           {/* TODO: add total games played and games won vs gave up */}
           <Bar options={chartOptions} data={chartData} />
           <Button onClick={resetStats} classes={[styles.resetButton]}>
-                        {t('stats.resetStatsButton')}
+            {t('stats.resetStatsButton')}
           </Button>
         </div>
       </>

commit 8b8c310d6e65bc3bf2884fb0aff61869bfc6a1be

Fixed hints
they were broken after 892d3f1
fixes #15

src/logic.ts
@@ -131,7 +131,7 @@ export const showHint = (hint: number) => {
   const currentHint = normalize(title, true);
 
   const hintSetting = getSettings().hintSetting;
-  console.log('Hint setting: '+hintSetting);
+  console.log('Hint number: '+hint+' Hint setting: '+hintSetting);
   let updatedHint = '';
   let nonSpaceChars = 0;
   const words = currentHint.split(' ');

src/pages/Game.tsx
@@ -150,7 +150,10 @@ class Game extends React.Component<
 
   giveHint = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    this.setState((prevState) => ({ hint: showHint(prevState.hintCount + 1) || '' }));
+    this.setState((prevState) => ({
+      hint: showHint(prevState.hintCount + 1) || '',
+      hintCount: prevState.hintCount + 1,
+    }));
   };
 
   giveUp = () => {